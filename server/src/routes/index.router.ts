4d388d4fcdc631671adcacd0dc600bec1ad20baf
Ebayart <eliott.bayart@etu.unilim.fr>
2025-12-17 11:47:21 +0100

    feat: we can now create documents and blocks

--- server/src/routes/index.router.ts
+++ server/src/routes/index.router.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import HomeController from '../controllers/home.controller';
+import documentsRouter from './documents.router';
 
 const router = express.Router();
 const homeController = new HomeController();
 
 /* GET home page. */
 router.get('/', homeController.renderHome);
+router.use('/documents', documentsRouter);
 
 export default router;
