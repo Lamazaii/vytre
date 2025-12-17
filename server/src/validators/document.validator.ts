37359622d287ac3a6850b836ba613df56522e591
Ebayart <eliott.bayart@etu.unilim.fr>
2025-12-17 11:29:26 +0100

    feat: add validator for frontend data

---
+++ server/src/validators/document.validator.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+// on définit à quoi ressemble un bloc valide
+const blockSchema = z.object({
+  text: z.string().min(1, "Le texte est obligatoire"),
+  step: z.number().int().positive("L'étape doit être un entier positif"),
+  nbOfRepeats: z.number().int().min(1, "Le nombre de répétitions doit être au moins 1"),
+});
+
+// on définit le Document entier
+export const createDocumentSchema = z.object({
+  title: z.string().min(1, "Le titre ne peut pas être vide"),
+  version: z.string().min(1, "La version est requise"), 
+  
+  // On dit que blocks est un tableau (array) contenant des objets qui respectent le blockSchema
+  // .optional() signifie qu'on a le droit de créer un document vide sans blocs pour commencer
+  blocks: z.array(blockSchema).optional(),
+});
+
