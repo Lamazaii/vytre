0405a1a337d6ddccc6b173c7c1013e0cc66d3aff
tsacristan <159041208+tsacristan@users.noreply.github.com>
2026-01-08 15:33:42 +0100

    overlay delete blocs fixed

--- client/src/components/popup/DeletePopup.vue
+++ client/src/components/popup/DeletePopup.vue
@@ -53,8 +53,9 @@ function onConfirm() {
 
 <style scoped>
 .popup-overlay {
-  position: absolute;
+  position: fixed;
   inset: 0;
+  
   background-color: rgba(0, 0, 0, 0.5);
   display: flex;
   align-items: center;
