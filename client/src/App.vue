06b39c4f6be325a52e1e26cbbc45a91bf8026e74
betton <yael.betton@etu.unilim.fr>
2026-01-12 10:16:22 +0100

    Add lector mod

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,24 +2,28 @@
 import { ref } from 'vue'
 import Menu from './components/applications/menu.vue'
 import Editor from './components/applications/editor.vue'
+import Lector from './components/applications/lector.vue'
 
 const currentMode = ref<'menu' | 'editor' | 'reader'>('menu')
 
-function selectMode(mode: 'editor' | 'reader') {
+function selectMode(mode: 'editor' | 'reader' | 'menu') {
   currentMode.value = mode
 }
 </script>
 
 <template>
   <Menu v-if="currentMode === 'menu'" @selectMode="selectMode" />
-  <Editor v-else-if="currentMode === 'editor'" />
+  <Editor v-else-if="currentMode === 'editor'"/>
+  <Lector v-else-if="currentMode === 'reader'" @selectMode="selectMode" class="lector"/>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+.lector {
+  max-width: 911px;
+  width : 911px;
+  height: 100%;
+  max-height: 717px;
+  min-height: 717px;
 }
 
 #app.no-scroll {
